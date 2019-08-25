06b2c006889aa59e017af919c7b6681478de292e
alx00 <alx00@DESKTOP-9V2SNNT.wireless-1x.unc.edu>
2019-08-25 14:39:37 -0400

    First Try

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -8,7 +8,5 @@ public class A1Adept {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,41 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Read in total number of customers
 		
+				int totalCustomers = scan.nextInt();
+				
+				// Array to store numbers
+				
+				int[] cstmr = new int[totalCustomers];
+				
+				// Read Customer's First Name, Last Name, and Number of Item Bought
+				
+				for (int i=0; i<cstmr.length; i++) {
+					String firstname = scan.next();
+					String lastname = scan.next();
+					int itemsBought = scan.nextInt();
+					char firstInitial = firstname.charAt(0);
+					double totalPrice = 0.0;
+					
+					for (int c=0; c<itemsBought; c++) {
+						int numberOfItem = scan.nextInt();
+						String nameOfItem = scan.next();
+						double priceOfOne = scan.nextDouble();
+						
+						double[] totalOfOne = new double[itemsBought];
+						totalOfOne[c] = priceOfOne*numberOfItem;
+						
+						for (int l=0; l<totalOfOne.length; l++) {
+							totalPrice = totalPrice + totalOfOne[l];
+						}
+				}
+				System.out.println(firstInitial + ". " + lastname + ": " + String.format("%.2f", totalPrice));
+		}
+				
+	scan.close();
+	
+	// Print results
+	
 	}
 }
